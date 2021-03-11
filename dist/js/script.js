b772dcd7f61f842be7e1165fe3bb7037859bc317
Denis <belovden2007@gmail.com>
2021-03-11 03:53:08 +0300

    final landing page

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,19 +2,19 @@ window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.header__nav'),
     menuItem = document.querySelectorAll('.header__nav__link'),
     hamburger = document.querySelector('.header__hamburger'),
-    store = document.querySelector('.phone__store');
+    store = document.querySelector('.phone_header');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('header__hamburger_active');
         menu.classList.toggle('header__nav_active');
-        store.classList.toggle('phone__store_active');
+        store.classList.toggle('phone_header_active');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('header__hamburger_active');
             menu.classList.toggle('header__nav_active');
-            store.classList.toggle('phone__store_active');
+            store.classList.toggle('phone_header_active');
         });
     });
 });
